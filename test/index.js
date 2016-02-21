878711706ce6445492c232d206f95b311d68df6a
jaws4d <jaws4d@gmail.com>
2016-02-21 18:52:55 -0500

    completed initial cut

--- test/index.js
+++ test/index.js
@@ -4,6 +4,13 @@ import Stateful from '../lib'
 
 test('State Creation', (test) => {
 
+  var o = {};
+  var cloned = _.clone(o);
+  var stateful = Stateful(o);
+
+  test.equal(typeof stateful.push, 'function', 'stateful can push');
+  test.equal(typeof stateful.pop, 'function', 'stateful can pop');
+  test.deepEqual(cloned, o, 'object is not allowed from being stateful');
   test.end();
 
 });
