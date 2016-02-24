dfe581446c7161150671a04e8358f25cb61144e3
jaws4d <jaws4d@gmail.com>
2016-02-23 22:54:38 -0500

    added transition and rewind as high-level state management functions

--- test/index.js
+++ test/index.js
@@ -9,11 +9,15 @@ test('State Creation', (test) => {
   var stateful = Stateful(o);
   var stateful2 = Stateful.create(o);
 
-  test.equal(typeof stateful.push, 'function', 'stateful can push');
-  test.equal(typeof stateful.pop, 'function', 'stateful can pop');
+  test.equal(typeof stateful.pushState, 'function', 'stateful can push one state');
+  test.equal(typeof stateful.popState, 'function', 'stateful can pop one state');
+  test.equal(typeof stateful.pushStates, 'function', 'stateful can push many states');
+  test.equal(typeof stateful.popStates, 'function', 'stateful can pop many states');
   test.deepEqual(cloned, o, 'object is not allowed from being stateful');
 
-  test.deepEqual(stateful.__original__, stateful2.__original__, 'create and a method invocation both create Stateful objects');
+  test.deepEqual(stateful.__original__,
+    stateful2.__original__,
+    'create and a method invocation both create Stateful objects');
   test.end();
 
 });
@@ -45,13 +49,13 @@ test('State Events', (test) => {
 
   var stateful = Stateful(o);
 
-  stateful.push(stateEvents);
+  stateful.pushState(stateEvents);
   test.equal(events.onInvoke, true, 'onInvoke is called');
   test.equal(events.onEnter, true, 'onEnter is called');
   test.equal(events.onDecorate, true, 'onDecorator is called');
   test.equal(events.onExit, false, 'onExit is not called, yet');
 
-  stateful.pop();
+  stateful.popState();
   test.equal(events.onExit, true, 'onExit is called');
   test.end();
 
@@ -77,10 +81,10 @@ test('State Pushing', (test) => {
 
   var stateful = Stateful(o);
 
-  stateful.push(state);
-  stateful.push(state);
-  stateful.push(state);
-  stateful.push(state);
+  stateful.pushState(state);
+  stateful.pushState(state);
+  stateful.pushState(state);
+  stateful.pushState(state);
 
   test.equal(o.a, 5, 'onEnter x4, onInvoke x1');
   test.equal(o.b, 1, 'onInvoke x1');
@@ -117,15 +121,66 @@ test('State Decoration', (test) => {
 
   var stateful = (Stateful(o));
 
-  stateful.push(state);
+  stateful.pushState(state);
   test.equal(o.a(), 3, 'a() decorated');
   test.equal(o.b(), 4, 'b() decorated');
   test.equal(o.value, 1, 'value decorated');
 
-  stateful.push(state);
+  stateful.pushState(state);
   test.equal(o.a(), 5, 'a() decorated');
   test.equal(o.b(), 6, 'b() decorated');
   test.equal(o.value, 2, 'value decorated');
+
+  test.end();
+
+});
+
+test('Transitions', (test) => {
+
+  var o = {};
+  var stateSets = _.times(3, function(j) {
+    return _.times(4, function(i) {
+      return {
+        onDecorate: function(original) {
+          var r = {};
+          r['value-' + ((j * 4) + i)] = 'value-' + ((j * 4) + i);
+          return r;
+        }
+      };
+    });
+  });
+
+  var stateful = (Stateful(o));
+
+  stateful.transition(stateSets[0]);
+  _.each(_.times(4), function(oo, k) {
+    var offset = 0;
+    test.equal(o['value-' + (offset + k)], 'value-' + (offset + k), 'sets of values-' + (offset + k));
+  });
+
+  stateful.transition(stateSets[1]);
+  _.each(_.times(4), function(oo, k) {
+    var offset = 4;
+    test.equal(o['value-' + (offset + k)], 'value-' + (offset + k), 'sets of values-' + (offset + k));
+  });
+
+  stateful.transition(stateSets[2]);
+  _.each(_.times(4), function(oo, k) {
+    var offset = 8;
+    test.equal(o['value-' + (offset + k)], 'value-' + (offset + k), 'sets of values-' + (offset + k));
+  });
+
+  stateful.rewind();
+  _.each(_.times(4), function(oo, k) {
+    var offset = 4;
+    test.equal(o['value-' + (offset + k)], 'value-' + (offset + k), 'sets of values-' + (offset + k));
+  });
+
+  stateful.rewind();
+  _.each(_.times(4), function(oo, k) {
+    var offset = 0;
+    test.equal(o['value-' + (offset + k)], 'value-' + (offset + k), 'sets of values-' + (offset + k));
+  });
 
   test.end();
 
